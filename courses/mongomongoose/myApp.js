397df4de3f935fd1275eaf1a5e063c569eeff601
Vin E. R. Yap <vineryap@gmail.com>
2021-12-26 23:12:01 +0900

    #9 Perform New Updates on a Document Using model.findOneAndUpdate()

--- courses/mongomongoose/myApp.js
+++ courses/mongomongoose/myApp.js
@@ -76,7 +76,15 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, updatedData) => {
+      if (err) console.log(err);
+      done(null, updatedData);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
