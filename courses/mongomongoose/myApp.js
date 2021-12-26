48559bb404b9471139cd003785a4e96417145bea
Vin E. R. Yap <vineryap@gmail.com>
2021-12-26 22:14:55 +0900

    #5 Use model.find() to Search Your Database

--- courses/mongomongoose/myApp.js
+++ courses/mongomongoose/myApp.js
@@ -36,7 +36,11 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) console.log(err);
+    else console.log(`Found ${personName}`);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
